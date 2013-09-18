43c585dd436907210752109de17b7c7a1edf9a8e
palkan <dementiev@webils.ru>
2013-09-18 19:48:56 +0400

    add load_config function

--- src/ulitos.erl
+++ src/ulitos.erl
@@ -6,7 +6,7 @@
 -author("palkan").
 
 %% API
--export([timestamp/0,get_var/3,print_stacktrace/0]).
+-export([timestamp/0,get_var/3,print_stacktrace/0, load_config/2]).
 
 %% @doc Return current UTC time in ms (uses <code>os:timestamp/0</code>).
 %% @end
@@ -40,4 +40,26 @@ print_stacktrace() ->
     smth:bark()
   catch
     _:_ -> M:F("Print stack:~n ~p~n",[erlang:get_stacktrace()])
+  end.
+
+
+%% @doc
+%% Set application <code>App</code> environment vars from file with name <code>File</code> located in app priv_dir.
+%% @end
+
+
+-spec load_config(atom(),list()) -> ok.
+
+load_config(App,File) ->
+  Path = code:priv_dir(App)++"/"++File,
+  Env = load_file_config(Path),
+  io:format("env from file:~p",[Env]),
+  [application:set_env(App, Key, Value) || {Key, Value} <- Env],
+  ok.
+
+
+load_file_config(Path) ->
+  case file:consult(Path) of
+    {ok, Env} -> Env;
+     _ -> []
   end.
