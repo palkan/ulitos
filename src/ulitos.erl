767b8ddfe517648a1a581978899bb4e3b5fdf4da
palkan <dementiev@webils.ru>
2013-09-23 21:46:53 +0400

    add random_string function

--- src/ulitos.erl
+++ src/ulitos.erl
@@ -6,7 +6,7 @@
 -author("palkan").
 
 %% API
--export([timestamp/0,get_var/3,print_stacktrace/0, load_config/2]).
+-export([timestamp/0,get_var/3,print_stacktrace/0, load_config/2, random_string/1]).
 
 %% @doc Return current UTC time in ms (uses <code>os:timestamp/0</code>).
 %% @end
@@ -63,3 +63,17 @@ load_file_config(Path) ->
     {ok, Env} -> Env;
      _ -> []
   end.
+
+
+
+%% @doc
+%% Simple random string generator (using <code>random:uniform/1</code> and latin characters).
+%% @end
+
+-spec random_string(Length::non_neg_integer()) -> string().
+
+random_string(0) -> [];
+
+random_string(Length) -> [random_char() | random_string(Length-1)].
+
+random_char() -> random:uniform(97) + 25 .
